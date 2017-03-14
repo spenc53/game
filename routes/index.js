390752165405664232451b1d6eba4940d629f643
Spencer Seeger <spencer.seeger@gmail.com>
2017-03-14 03:05:45 +0000

    THing

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http').Server(express);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index.html', { root: 'public' });
+router.get('/', function(req, res){
+  res.sendFile('/index.html', { root: 'public' });
 });
 
-//router.get('/commands',function(req,res){
-//  res.send();
-//}
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
 
 module.exports = router;
